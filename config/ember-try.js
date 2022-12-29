7e89754c8c09839d705a0eb97aa33f2bb9e32533
Ryan Tablada <ryan.tablada@gmail.com>
2022-12-29 17:05:52 -0600

    feat: add ember-try

---
+++ config/ember-try.js
@@ -0,0 +1,36 @@
+'use strict';
+
+module.exports = async function () {
+  return {
+    scenarios: [
+      {
+        name: 'ember-lts-4.0',
+        npm: { devDependencies: { 'ember-source': '~4.0' } },
+      },
+      {
+        name: 'ember-lts-4.4',
+        npm: { devDependencies: { 'ember-source': '~4.4' } },
+      },
+      {
+        name: 'ember-lts-3.28',
+        npm: { devDependencies: { 'ember-source': '~3.28' } },
+      },
+      {
+        name: 'ember-lts-3.24',
+        npm: { devDependencies: { 'ember-source': '~3.24' } },
+      },
+      {
+        name: 'ember-lts-3.20',
+        npm: { devDependencies: { 'ember-source': '~3.20' } },
+      },
+      {
+        name: 'ember-lts-3.16',
+        npm: { devDependencies: { 'ember-source': '~3.16' } },
+      },
+      {
+        name: 'ember-lts-3.12',
+        npm: { devDependencies: { 'ember-source': '~3.12' } },
+      },
+    ],
+  };
+};
