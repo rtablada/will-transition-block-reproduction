cef8aab87d7d87ffd97587bf73a4980266306088
Ryan Tablada <ryan.tablada@gmail.com>
2022-12-29 16:53:38 -0600

    bring over tests and updates to app from 3-24 branch

---
+++ tests/acceptance/transition-blocking-test.js
@@ -0,0 +1,69 @@
+import { module, test } from 'qunit';
+import { visit, currentURL, click, find } from '@ember/test-helpers';
+import { setupApplicationTest } from 'ember-qunit';
+
+module('Acceptance | Route willTransition promise awareness', function (hooks) {
+  setupApplicationTest(hooks);
+
+  const getModal = () => find('[data-test-confirm-modal]');
+
+  test('Route willTransition should block transitions from continuing if it returns a promise', async function (assert) {
+    await visit('/route-will-transition');
+
+    assert.strictEqual(currentURL(), '/route-will-transition');
+
+    await click('[data-test-nav="route-will-transition"]');
+
+    assert.strictEqual(
+      currentURL(),
+      '/route-will-transition',
+      'Url should not have changed after clicking current route link'
+    );
+
+    assert.notOk(
+      getModal(),
+      'The confirmation modal should not be shown if the user is staying on the same route'
+    );
+
+    await click('[data-test-nav="route-will-change-event"]');
+
+    assert.ok(
+      getModal(),
+      'The confirmation modal should be visible when navigating away'
+    );
+
+    assert.strictEqual(
+      currentURL(),
+      '/route-will-transition',
+      'Url should not have changed after clicking current route link'
+    );
+
+    await click('[data-test-confirm-modal-action="cancel"]');
+
+    assert.strictEqual(
+      currentURL(),
+      '/route-will-transition',
+      'Transition should be aborted after clicking cancel'
+    );
+
+    assert.notOk(
+      getModal(),
+      'The confirmation modal should be hidden after clicking cancel'
+    );
+
+    await click('[data-test-nav="route-will-change-event"]');
+
+    await click('[data-test-confirm-modal-action="ok"]');
+
+    assert.strictEqual(
+      currentURL(),
+      '/route-will-change-event',
+      'Transition should continue after ok button is clicked'
+    );
+
+    assert.notOk(
+      getModal(),
+      'The confirmation modal should be hidden after ok button is clicked'
+    );
+  });
+});
